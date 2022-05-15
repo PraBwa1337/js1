34962061349a204a61451836532902088856e72f
PraBwa1337 <danik020508@gmail.com>
2022-05-15 14:21:42 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+// let num = 5;
+// for (let i = 1; i < 11; i++){
+//     console.log(`${i} * ${num} = ${num * i}`)
+// }
+
+// let arr = [1,2,3,4,5,-1,-3,-5];
+// let sum = 0;
+// for (let i = 0; i < arr.length; i++){
+//     if (arr[i] > 0) {
+//         sum = sum + arr[i];
+//     }
+// }
+// console.log(sum);
+
+// let x = 1;
+// console.log(-x);
+
+let arr = [1,2,3,4,5,6,7,4,2];
+for (let i = 0;i < arr.length;i++){
+    if (arr[i] %2 === 0){
+        console.log(arr[i] + 1)
+    } else{
+        console.log(arr[i] + 1)
+    }
+}
